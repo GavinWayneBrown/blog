f0b6d8e546c61070bb30947c5b16f539b54eb00b
Gavin <gavinwaynebrown@gmail.com>
2024-10-09 23:10:50 -0600

    forms, user accounts, and static files

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 
 
 # django_project/urls.py
-from django.contrib import admin
-from django.urls import path, include # new
-
+from django.contrib import admin 
+from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),  # new
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),  # new
+    path("", include("blog.urls")),
 ]
